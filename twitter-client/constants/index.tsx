dcea3dbe7144854813f2177939b3c013b3e9aa6a
Haseeb Yousuf <haseebyousuff@gmail.com>
2024-01-19 00:12:26 +0530

    Add new icons to sidebar menu

--- twitter-client/constants/index.tsx
+++ twitter-client/constants/index.tsx
@@ -3,10 +3,9 @@ import React from 'react';
 import { BiHomeCircle, BiUser } from 'react-icons/bi';
 import { BsBell, BsBookmark, BsEnvelope } from 'react-icons/bs';
 import { CiSearch } from 'react-icons/ci';
-// interface TwitterSidebarButton {
-//   title: string;
-//   icon: React.ReactNode;
-// }
+import { MdOutlineLibraryBooks } from 'react-icons/md';
+import { RiGroupLine } from 'react-icons/ri';
+
 export const sidebarMenuItems: TwitterSidebarButton[] = [
   {
     title: 'Home',
@@ -20,9 +19,18 @@ export const sidebarMenuItems: TwitterSidebarButton[] = [
     title: 'Notification',
     icon: <BsBell />,
   },
+
   {
     title: 'Messages',
     icon: <BsEnvelope />,
+  },
+  {
+    title: 'Lists',
+    icon: <MdOutlineLibraryBooks />,
+  },
+  {
+    title: 'Communities ',
+    icon: <RiGroupLine />,
   },
   {
     title: 'Bookmarks',
