966d45a12464dbd2e898b444eef02d22ae682f47
Haseeb Yousuf <haseebyousuff@gmail.com>
2024-01-19 00:41:19 +0530

    Update home page layout

--- twitter-client/app/(home)/page.tsx
+++ twitter-client/app/(home)/page.tsx
@@ -3,7 +3,9 @@ import React from 'react';
 
 export default function Home() {
   return (
-    <div className='w-full h-full '>
+    <div className='w-full h-screen overflow-scroll '>
+      <FeedCard />
+      <FeedCard />
       <FeedCard />
     </div>
   );
