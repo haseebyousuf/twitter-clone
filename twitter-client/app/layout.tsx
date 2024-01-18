a0127817e8ed40ea2703014093ae36eedc0a1a5e
Haseeb Yousuf <haseebyousuff@gmail.com>
2024-01-19 00:41:27 +0530

    Add smooth scrolling to HTML element

--- twitter-client/app/layout.tsx
+++ twitter-client/app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang='en'>
+    <html lang='en' className='scroll-smooth'>
       <body className={`${inter.variable} `}>
         {/* {children} */}
         <div className='grid grid-cols-12 h-screen w-screen px-20'>
