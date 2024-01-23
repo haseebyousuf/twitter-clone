d95df5b08991f8a35914c7c489e85b4f97c9416a
Haseeb Yousuf <haseebyousuff@gmail.com>
2024-01-23 16:46:06 +0530

    Add Google login functionality to RightSidebar component

--- twitter-client/components/shared/RightSidebar.tsx
+++ twitter-client/components/shared/RightSidebar.tsx
@@ -1,7 +1,17 @@
-import React from 'react';
-
+'use client';
+import React, { useCallback } from 'react';
+import { CredentialResponse, GoogleLogin } from '@react-oauth/google';
 const RightSidebar = () => {
-  return <div className='col-span-3'>Right Sidebar</div>;
+  const handleGoogleLogin = useCallback((cred: CredentialResponse) => {}, []);
+
+  return (
+    <div className='col-span-3 p-5'>
+      <div className='p-5 bg-slate-950 rounded-lg'>
+        <h1 className='my-2 text-2xl'>New to twitter?</h1>
+        <GoogleLogin onSuccess={(cred) => console.log(cred)} />
+      </div>
+    </div>
+  );
 };
 
 export default RightSidebar;
